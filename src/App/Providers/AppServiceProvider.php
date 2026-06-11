e189b90b97a8b55ade4f9a6752e1da8f20a65e37
WepX <wepalytoylyyew@gmail.com>
2026-06-12 00:43:58 +0500

    added URL::forceScheme('https');

--- src/App/Providers/AppServiceProvider.php
+++ src/App/Providers/AppServiceProvider.php
@@ -4,23 +4,22 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         View::composer('*', function ($view) {
             $view->with('user', Auth::user());
         });
