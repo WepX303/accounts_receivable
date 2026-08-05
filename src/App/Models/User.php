2ece216a4e5547e755fd4d185a6abfdf8df2268a
WepX <wepalytoylyyew@gmail.com>
2026-08-05 16:19:49 +0500

    fix v2

--- src/App/Models/User.php
+++ src/App/Models/User.php
@@ -8,11 +8,10 @@ use App\Enums\UserRoleEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'firstname',
